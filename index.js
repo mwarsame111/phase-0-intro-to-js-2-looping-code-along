f5b06692cf8d1268ef4e367d05238e489f91fa68
mohamed warsame <mwarsame111@gmail.com>
2023-07-04 15:28:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(array, string){
+  const newArray = []
+  for(let i = 0 ; i < array.length; i++){
+  newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+  }
+  return newArray;
+}
+function countDown(n){
+  while (n >= 0){
+    console.log(n--);
+  }
+}
